310e0df3840b7d7ca93c87ae35381c524f340768
hyeongyu-data <hyeongyu.data@gmail.com>
2026-06-01 15:46:36 +0900

    Feat: thinking 노드만 구성

--- langgraph/lg_rag_agent.py
+++ langgraph/lg_rag_agent.py
@@ -33,18 +33,18 @@ from tools import rag_search                       # rag 도구를 외부에 구
 load_dotenv()
 
 # LLM 모델 구성 ChatBedrockConverse
-llm = ChatBedrockConverse(model  = os.getenv('MODEL_ID'),
-                            max_tokens = 1000,
-                            temperature = 0.5,
-                            region_name = os.getenv('AWS_REGION') 
-                        )
+llm = ChatBedrockConverse(  model       = os.getenv('MODEL_ID'), 
+                            max_tokens  = 1000,
+                            temperature = 0.5,                            
+                            region_name = os.getenv('AWS_REGION')
+                        ) 
 
 # 툴 등록한 LLM 모델 획득
 tools = [ rag_search ]
 llm_with_tools = llm.bind_tools( tools )
 
-# 퓨샷 프롬프트 구성
-# 샘플의 형태는 다양할 수 있다
+# 퓨샷 프럼프트 구성
+# 샘플의 형태는 다양할 수 있다.
 examples        = [
     {"input":"비 오는날 국물이 땡겨", "output":"국룰이죠. 칼국수와 잔치국수가 좋습니다."},
     {"input":"다이어트를 위해서 오늘 칼로리가 낮은것으로", "output":"관리하시는 군요. 닭가슴 샐러드 드세요."},
@@ -57,50 +57,62 @@ few_shot_prompt = FewShotChatMessagePromptTemplate(
     examples    = examples,
     example_prompt = example_prompt
 )
-final_prompt    = ChatPromptTemplate.format_messages([
+final_prompt    = ChatPromptTemplate.from_messages([
     # 1. 페르소나
-    ('system','당신은 센스 있는 식사 메뉴 추천 전문가입니다. 사용자의 상황에 맞춰서 메뉴를 추천하고, 필요하면 도구를 사용하여 실제 식당을 찾으세요.'),
+    ('system', '당신은 센스 있는 식사 메뉴 추천 전문가입니다. 사용자의 상황에 맞춰서 메뉴를 추천하고, 필요하면 도구를 사용하여 실제 식당을 찾으세요.'),
     # 2. 퓨샷 샘플
     few_shot_prompt
     # 3. 사용자 질문
-    ('human','{messages}')
+    ('human', '{query}')
 ])
 
 # 랭그래프 상태 구성
 class AgentState(TypedDict):
-    messages: List[BaseMessage]
+    messages: List[ BaseMessage ]
 
 # 노드 정의
-# 사용자의 질의(대화)를 보고, 생각하는 단계 구성(메뉴 추천, 도구 사용 결정)
+# 사용자의 질의(대화)를 보고, 생각하는 단계 구성 (메뉴 추천, 도구 사용 결정)
 def thinking_node( state:AgentState ):
-    pass
+    # 사용자 입력내용 추출(획득)
+    msg = state['messages']  # 사용자 -> UI -> 입력 -> 엔터 -> 서버 -> 랭그래프객체.invoke(프럼프트)
+    # 랭체인 구성 (일반향) : 퓨샷 프럼프트(페르소나, 샘플 구성되어 있음)
+    chain    = final_prompt | llm_with_tools
+    # 1차 추론 요청 진행
+    res      = chain.invoke( {"query":msg})
+    # 응답 결과 반환
+    return {'messages':[ res ]}
 
-# 툴, 도구를 사용하기로 결정했다면, 실제 도구 제공 -> 외부기능 -> ... -> MCP연계
+# 툴, 도구를 사용하기로 결정했다면, 실제 도구 제공 -> 외부기능 -> .... => MCP 연계 
 def tool_node( state:AgentState ):
     pass
 
 # 검색 결과를 바탕으로 최종 답변 추론
-# 실제는 다시 thinking_node로 다시 가서 최종 구성해도 됨.
+# 실제는 다시 thinking_node로 다시 가서 최종 구성해도됨.
 def final_answer_node( state:AgentState ):
     pass
 
 # 랭그래프 연결
 workflow = StateGraph(AgentState)
 workflow.add_node('thinking',       thinking_node)
-workflow.add_node('tool',           tool_node)
-workflow.add_node('final_answer',   final_answer_node)
-workflow.set_entry_point(thinking_node) # 최초 프롬프트를 가지고 추론 진행(직접 ok, 도구 ok)
+#workflow.add_node('tool',           tool_node)
+#workflow.add_node('final_answer',   final_answer_node)
+workflow.set_entry_point(thinking_node) # 최초 프럼프트를 가지고 추론 진행(직접 ok, 도구 ok)
 # 조건부 엣지
 # LLM 호출을 통해서 답변 마무리, 도구를 이용하여 마무리 할지등
-def custom_check_tool_node(state:AgentState):
-    pass
-workflow.add_conditional_edges('thinking',custom_check_tool_node)
-workflow.add_edge('tool', 'final_answer')   # 도구 사용 -> 최종답변노드, 방향성설정
-workflow.add_edge("final_answer", END)      # 그래프의 끝 지정
+#def custom_check_tool_node(state:AgentState):
+#    pass
+#workflow.add_conditional_edges('thinking', custom_check_tool_node)
+#workflow.add_edge('tool', 'final_answer') # 도구 사용 => 최종답변노드, 방향성설정
+#workflow.add_edge("final_answer", END)    # 그래프이 끝 지정
+workflow.add_edge("thinking_node", END)
 
 # 흐름 시나리오
-# 프롬프트 -> thinking -> END
-# 프롬프트 -> thinking -> 부족함 -> tool -> rag -> final_answer -> END
+# 프럼프트 => thinking => END
+# 프럼프트 => thinking => 부족함 => tool => rag => final_answer => END
 
 # 랭그래프객체
 랭그래프객체 = workflow.compile()
+
+if __name__ == '__main__':
+    res = 랭그래프객체.invoke( { "messages": "가벼운 식사" } )  # 가벼운 식사 => 사용자 입력
+    print(  res )
