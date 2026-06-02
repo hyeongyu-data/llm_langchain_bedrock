e609367b430c26c0d5fcfff1bdd36591e5bbb322
hyeongyu-data <hyeongyu.data@gmail.com>
2026-06-02 09:25:45 +0900

    Hotfix: thinking 노드만 확인 수정

--- langgraph/lg_rag_agent.py
+++ langgraph/lg_rag_agent.py
@@ -49,10 +49,10 @@ examples        = [
     {"input":"비 오는날 국물이 땡겨", "output":"국룰이죠. 칼국수와 잔치국수가 좋습니다."},
     {"input":"다이어트를 위해서 오늘 칼로리가 낮은것으로", "output":"관리하시는 군요. 닭가슴 샐러드 드세요."},
 ]
-example_prompt  = ChatPromptTemplate.from_messages(
+example_prompt  = ChatPromptTemplate.from_messages([
     ('human', '{input}'  ),
     ('ai',    '{output}' ),
-)
+])
 few_shot_prompt = FewShotChatMessagePromptTemplate(
     examples    = examples,
     example_prompt = example_prompt
@@ -61,7 +61,7 @@ final_prompt    = ChatPromptTemplate.from_messages([
     # 1. 페르소나
     ('system', '당신은 센스 있는 식사 메뉴 추천 전문가입니다. 사용자의 상황에 맞춰서 메뉴를 추천하고, 필요하면 도구를 사용하여 실제 식당을 찾으세요.'),
     # 2. 퓨샷 샘플
-    few_shot_prompt
+    few_shot_prompt,
     # 3. 사용자 질문
     ('human', '{query}')
 ])
@@ -89,6 +89,9 @@ def tool_node( state:AgentState ):
 # 검색 결과를 바탕으로 최종 답변 추론
 # 실제는 다시 thinking_node로 다시 가서 최종 구성해도됨.
 def final_answer_node( state:AgentState ):
+    msg = state['messages']
+    res = llm.invoke( msg ) # 툴이 필요 없거나, 툴을 사용했거나 => 답변에 충족하는 내용을 가지고 있으므로 llm으로 처리
+    return {'messages':[ res ]}
     pass
 
 # 랭그래프 연결
@@ -96,7 +99,7 @@ workflow = StateGraph(AgentState)
 workflow.add_node('thinking',       thinking_node)
 #workflow.add_node('tool',           tool_node)
 #workflow.add_node('final_answer',   final_answer_node)
-workflow.set_entry_point(thinking_node) # 최초 프럼프트를 가지고 추론 진행(직접 ok, 도구 ok)
+workflow.set_entry_point('thinking') # 최초 프럼프트를 가지고 추론 진행(직접 ok, 도구 ok)
 # 조건부 엣지
 # LLM 호출을 통해서 답변 마무리, 도구를 이용하여 마무리 할지등
 #def custom_check_tool_node(state:AgentState):
@@ -104,7 +107,7 @@ workflow.set_entry_point(thinking_node) # 최초 프럼프트를 가지고 추�
 #workflow.add_conditional_edges('thinking', custom_check_tool_node)
 #workflow.add_edge('tool', 'final_answer') # 도구 사용 => 최종답변노드, 방향성설정
 #workflow.add_edge("final_answer", END)    # 그래프이 끝 지정
-workflow.add_edge("thinking_node", END)
+workflow.add_edge("thinking", END)
 
 # 흐름 시나리오
 # 프럼프트 => thinking => END
