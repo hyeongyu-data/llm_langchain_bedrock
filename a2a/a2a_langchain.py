a38766e68732d86e01a44771bc8264fe292fb060
hyeongyu-data <hyeongyu.data@gmail.com>
2026-06-02 13:33:29 +0900

    Feat: State 형태 정의

--- a2a/a2a_langchain.py
+++ a2a/a2a_langchain.py
@@ -1,4 +1,4 @@
-# 1. 모듈 가져오기
+# 1. 모듈가져오기
 import boto3
 from langchain_aws import ChatBedrock
 from langchain_core.prompts import ChatPromptTemplate
@@ -9,32 +9,33 @@ import os
 
 dotenv.load_dotenv()
 
-# LLM 생성, 차후 에이전트별로 역할에 따라 최적의 LLM 배치할 수 있음
-llm = ChatBedrock(model_id = os.getenv('MODEL_ID'),
-    client       = boto3.client('bedrock-runtime',region_name = os.getenv('AWS_REGION')),
-    model_kwargs = {"temperature":0.7} 
+# LLM 생성, 차후 에이전트별로 역활에 따라 최적의 LLM 배치 할수 있음
+# 모든 에이전트가 모두 사용. 페르소나에 따라 역활, 성능 조정할 수 있다
+llm = ChatBedrock( model_id = os.getenv('MODEL_ID'),
+    client       = boto3.client('bedrock-runtime', region_name=os.getenv('AWS_REGION') ),
+    model_kwargs = {"temperature":0.7}
 )
 
-# Agent 1, 신입 개발자를 위한 프롬프트 구성
+# Agent 1, 신입 개발자를 위한 프럼프트 구성
 developer_prompt = ChatPromptTemplate.from_messages([
-    ('system', '당신은 열정적인 "신입 파이썬 개발자"입니다. 요청받은 기능을 구현하는 코드를 작성하세요. 설명은 최소화하고 코드 위주로 작성하세요.'),
+    ('system', '당신은 열정적인 "신입 파이썬 개발자"입니다. 요청받은 기능을 구현하는 코드를 작성하세요. 설명은 최소화 하고 코드 위주로 작성하세요.'),
     ('user'  , '{request}'),
 ])
-
-# Agent 2, 전문 리뷰어를 위한 프롬프트 구성
+# Agent 2, 전문 리뷰어를 위한 프럼프트 구성
 reviewer_prompt = ChatPromptTemplate.from_messages([
-    ('system', '''당신은 까다로운 "전문 개발자"입니다. 신입 개발자가 작성한 코드를 리뷰하세요.
+    ('system', '''당신은 까다로운 "전문 개발자"입니다. 신입 개발자가 작성한 코드를 리뷰하세요. 
 보안 취약점, 비효율적인 부분, 스타일 가이드를 점검하고 수정 제안을 하세요.
-코드가 완벽하다면, "PASS"라고만 답하세요.
-'''),
+코드가 완벽하자면, "PASS"라고만 답하세요.
+'''
+     ),
     ('user'  , '다음 코드를 리뷰해주세요:\n\n{code}'),
 ])
-
-# Agent 3, (코드 리뷰를 기반으로 코드를 수정하는) 리파인더를 위한 프롬프트 구성
+# Agent 3, (코드 리뷰를 기반으로 코드를 수정하는 )리파인더를 위한 프럼프트 구성
 refiner_prompt = ChatPromptTemplate.from_messages([
     ('system', '당신은 열정적인 "신입 파이썬 개발자"입니다. 전문개발자의 리뷰를 보고 코드를 수정하여 다시 제출하세요.'),
-    ('user'  , '이전 코드:\n{original_code}\n\n리뷰내용:\n{feedback\n\n위 내용을 반영하여 개선된 전체 코드를 다시 작성하세요}'),
+    ('user'  , '이전 코드:\n{original_code}\n\n리뷰 내용:\n{feedback}\n\n위 내용을 반영하여 개선된 전체 코드를 다시 작성하세요'),
 ])
+
 
 # 랭체인 구성
 developer_agent = developer_prompt | llm | StrOutputParser()
@@ -42,19 +43,18 @@ reviewer_agent  = reviewer_prompt  | llm | StrOutputParser()
 refiner_agent   = refiner_prompt   | llm | StrOutputParser()
 
 # 협업 구성
-def run_agent_collaboration(topic):
-    # 1. 목표 로그(프롬프트) 출력
+def run_agent_collaboration( topic ):
+    # 1. 목표 로그(프럼프트) 출력
     print(f'목표 : {topic}\n' + '='*50)
 
     # 2. 신입 개발자 초안 작성
     print(f'\n[신입 개발자] 코드 작성 중..')
-    draft_code = developer_agent.invoke({"request":topic})
-    print(f'--\n{draft_code}')
+    draft_code = developer_agent.invoke( {"request":topic} )
+    #print(f'--\n{draft_code}')
 
-    # 3. 전문 개발자 리뷰 작성
-    print(f'\n[전문 개발자] 코드 검토 중..')
-    feedback = reviewer_agent.invoke({"code":draft_code})
-    print(f'--\n{feedback}')
+    # 3. 전문 개발자 리뷰 작성 -> PASS 가 나올때가지 반복 처리 가능 -> 반복문 사용 
+    print(f'\n[전문 개발자] 리뷰 검토 중..')
+    feedback = reviewer_agent.invoke( {"code":draft_code} )    
 
     if feedback == 'PASS':
         print(f'--[최종코드]--\n{draft_code}')
@@ -63,11 +63,10 @@ def run_agent_collaboration(topic):
         # 4. 신입 개발자 피드백 반영
         print(f'\n[신입 개발자] 피드백 반영하여 수정 중..')
         final_code = refiner_agent.invoke({
-            "original_code":draft_code,
+            "original_code":draft_code, 
             "feedback":feedback
         })
         print(f'--[최종코드]--\n{final_code}')
-
     pass
 
 # 구동
---
+++ a2a/a2a_langgraph.py
@@ -0,0 +1,55 @@
+'''
+- 에이전트 => 그래프상 노드
+- 에이전트 워크플로우 : 작성->리뷰->체크->수정작성->리뷰->체크... 
+    - 엣지 구성(규칙, 시작, 종료) : 
+       - 특정 횟수를 초과하면 종료, 
+       - 시작 노드 지정
+       - PASS 후 종료
+       - FAIL이면 다시 작성 -> 순환구조, 2개의 노드로 구성 완료 -> 신입 개발자 에이전트의 진화 (기억만 하면)
+- 아킥텍쳐
+    - Langgraph
+    - State : 대화 내용, 수정 횟수 저장하는 공유 메모리
+    - Node
+        - Coder Node    : 상태(메모리)를 읽고 코드 신규 생성/수정
+        - Reviewer Node : 코드를 검증하고 Pass/Fail 판정(리뷰 내용 전달)
+    - Edge
+        - entry_point      : Coder Node
+        - node_dir         : Coder Node -> Reviewer Node
+        - Conditional Edge : Reviewer Node를 타겟, Reviewer의 판정에 따라 Coder Node 갈지, End 갈지 결정
+
+'''
+# 1. 모듈가져오기
+import boto3
+from langchain_aws import ChatBedrock
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
+from langgraph.graph import StateGraph, END
+from typing import Annotated, List, TypedDict
+import operator # 수정 요청한 회수 계산을 위한 처리 => 리액트의 리듀서의 기능 담당(참고)
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+# 2. 상태 정의 (메세지-messages, 재시도횟수-iterations, AgentState)
+class AgentState(TypedDict):
+    # Annotated[ ... ] : 메타데이터 표기
+    # 타입힌트 제시, operator.add 전달 -> 상태 업데이트 방식 명확하게 지정 
+    # 덮어쓰지 말고, 기존 내용에 새로운 내용을 추가하라
+    # 대화를 기억하는 방법을 => 누적하여 보관
+    messages   : Annotated[ List[BaseMessage], operator.add ]
+    # 재시도 회수, 리뷰등을 통해 수정 내용 발생시 최대 순환 수정 회수 제한 비교용
+    iterations : int
+    
+# 3. LLM 정의 
+
+# 4. 노드 정의 -> Agent 정의
+
+# 5. 조건부 엣지 정의
+
+# 6. 그래프 구성
+
+# 7. 실행
+if __name__ == '__main__':
+    pass
