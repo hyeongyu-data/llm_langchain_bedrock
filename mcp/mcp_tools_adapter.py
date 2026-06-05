025d51a7e354139c2c2ccfd2b74207469848afbf
hyeongyu-data <hyeongyu.data@gmail.com>
2026-06-05 10:18:47 +0900

    Feat: 세션, 툴 목록 획득

--- mcp/mcp_tools_adapter.py
+++ mcp/mcp_tools_adapter.py
@@ -1,7 +1,7 @@
 '''
 MCP Server 와 통신
-MCP에서 정의한 Tool을 Langchain/Langgraph 용 Tool로 변환처리
-LLM이 해당 도구에 대한 이해와, 사용 판단에 정확한 정보를 제공
+MCP에서 정의한 Tool을 Langchain/Langgraph 용 Tool로 변환 처리
+LLM이 해당 도구에 대한 이해와 ,사용 판단에 정확한 정보를 제공
 '''
 # 1. 모듈 가져오기
 import asyncio
@@ -17,33 +17,68 @@ class MCPToolAdapter:
     '''MCP Server와 통신. LangChain/LangGraph Tool로 변환 제공'''
     # 생성자
     def __init__(self, server_script: str = 'server.py'):
-        self.server_script = server_script
-        self.tools         = [] # MCP 서버에게 툴 목록 가져와서 저장
-        self.read_stream   = None # 입력 스트림
-        self.write_stream  = None # 출력 스트림
-        self.sesstion: Optional[ClientSession] = None # 세션 멤버변수 -> 여러 함수에서 사용하겠다.
-        self._studio_context = None # 입출력에 관련한 내부적 프로세스 접근을 위하 컨텍스트
+        self.server_script  = server_script
+        self.mcp_tools      = []   #  mcp Tool
+        self.tools          = []   #  LangChain/LangGraph Tool
+        self.read_stream    = None # 입력 스트림 -> 여러 함수에서 사용하겠다.
+        self.write_stream   = None # 출력 스트림 -> 여러 함수에서 사용하겠다.
+        self.session: Optional[ClientSession] = None # 세션 맴버변수 -> 여러 함수에서 사용하겠다.
+        self._stdio_context = None # 입출력에 관련한 내부적 프로레스 접근을 위한 컨텍스트 
         pass
-
+    
     # 초기화
-    async def initialize(self):
+    async def initialize( self ):
         '''MCP Server 연결, Tool 로드'''
-        # Server 접속시 필요한 정보 세팅
+        # MCP Server 접속시 필요한 정보 세팅
         server_params = StdioServerParameters(
             command = sys.executable,
             args    = [self.server_script],
             env     = None
         )
-        # 메세지가 오염되면 -> 출력을 sys.stderr
+        # 메세지가 오염되면 => 출력을 sys.stderr
         print('MCP 서버 연결중..')
         try:
+            # 입력, 출력, 세션등, 여러 함수에서 사용한다면 with x
+            # MCP 서버를 접속할때 사용하는 내부 컨텍스트 객체
+            self._stdio_context = stdio_client(server_params)
+            # 내부 함수를 강제로 호출(내가 원하는 시점에 처리)
+            # 비동기, 입력/출력 스트림 연결하여 반환
+            stdio_tuple = await self._stdio_context.__aenter__()
+            # 입력/출력 스트 획득
+            if isinstance( stdio_tuple, tuple ):
+                self.read_stream, self.write_stream = stdio_tuple
+            else:
+                self.read_stream  = stdio_tuple
+                self.write_stream = stdio_tuple
+
+            # 세션 획득(생성) => 툴을 가져올수 있음
+            # JSON RPC 2.0 기준 상호 통신할수 있는 논리적인 상태(세션) 완성
+            self.session = ClientSession(self.read_stream, self.write_stream)
+            # 실제 활성화를 위해 직접 호출
+            await self.session.__aenter__()
+            # 세션 초기화
+            await self.session.initialize()
+            # MCP 서버에게 Tool 목록 요청
+            res = await self.session.list_tools()
+            self.mcp_tools = res.tools
+            print(f'MCP 서버로부터 { len(self.mcp_tools) }개의 툴 로드됨')
             
+            return self
         except Exception as e:
-            print('MCP 연결 실패', e)
+            print('MCP 서버 연결 실패', e)
             raise
         pass
     
-
+    async def cleanup(self):
+        '''입력/출력 스트림, 세션등 자원 해제(개발자 관리)'''
+        # 세션이 존재하면 -> 세션 종료
+        # 컨텍스트가 존재하면 -> 입력/출력 스트림 종료
+        try:
+            if self._stdio_context:
+                await self._stdio_context.__aexit__(None, None, None)
+        except Exception as e:
+            print('입력/출력 스트림 종료 에러', e)
+        pass
 
 # 4. 테스트
 if __name__ == '__main__':
